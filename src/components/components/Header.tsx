8834f6aa598828fcdc15ac4eed97e20973f43a5b
Nonelela <nonelelacele@gmail.com>
2022-06-06 11:48:55 +0200

    first commit

---
+++ src/components/components/Header.tsx
@@ -0,0 +1,10 @@
+import {FC} from 'react';
+const Header : FC = () => {
+    return (
+        <div className='header'>
+            <h3>Header</h3>
+        </div>
+    );
+}
+
+export default Header;
